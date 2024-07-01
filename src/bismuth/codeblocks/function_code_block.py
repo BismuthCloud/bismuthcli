a493df095740a2f3a7dcd524e34d18b589bac054
Nick Gregory <computerfreak97@gmail.com>
2024-07-01 17:15:08 -0400

    make functioncodeblock properly reflect types

--- src/bismuth/codeblocks/function_code_block.py
+++ src/bismuth/codeblocks/function_code_block.py
@@ -1,8 +1,12 @@
 from .base_code_block import BaseCodeBlock
-from typing import Callable, Any
+from typing import ParamSpec, TypeVar, Generic, Callable
 
 
-class FunctionCodeBlock(BaseCodeBlock):
+P = ParamSpec('P')
+R = TypeVar('R')
+
+
+class FunctionCodeBlock(BaseCodeBlock, Generic[P, R]):
     """
     Extends BaseCodeBlock. Run a python function with some requirements.
     """
@@ -10,10 +14,10 @@ class FunctionCodeBlock(BaseCodeBlock):
     # Flag for if this block makes any network requests.
     network_enabled: bool
     # Function that will be run by `execute()`.
-    func: Callable[..., Any]
+    func: Callable[P, R]
 
     def __init__(
-        self, func: Callable[..., Any], network_enabled: bool = False, *args, **kwargs
+        self, func: Callable[P, R], network_enabled: bool = False, *args, **kwargs
     ):
         """
         Initializes the FunctionCodeBlock with the function callable.
@@ -22,8 +26,11 @@ class FunctionCodeBlock(BaseCodeBlock):
         self.network_enabled = network_enabled
         self.func = func
 
-    def exec(self, *args, **kwargs: Any) -> Any:
+    def exec(self, *args: P.args, **kwargs: P.kwargs) -> R:
         """
         Run a subset of arbitrary python functions as defined by Bismuth provided args and kwargs.
         """
         return self.func(*args, **kwargs)
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        return self.exec(*args, **kwargs)
